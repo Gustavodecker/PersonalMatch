7f44dd61551566a87a41d18759a3b3682d344a9c
Gustavo Miguel <37581694+Gustavodecker@users.noreply.github.com>
2026-07-10 11:05:47 -0300

    Updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -2746,40 +2746,27 @@ const s = StyleSheet.create({
     elevation: 2,
   },
 
-headerInner: {
+ headerInner: {
 flexDirection: 'row',
 alignItems: 'center',
-justifyContent: 'flex-end',
-paddingLeft: 0,
-paddingRight: IS_DESKTOP ? 28 : 16,
-paddingVertical: IS_DESKTOP ? 6 : 6,
-minHeight: IS_DESKTOP ? 110 : 82,
+justifyContent: 'space-between',
+paddingHorizontal: IS_DESKTOP ? 24 : 16,
+paddingVertical: IS_DESKTOP ? 8 : 6,
+minHeight: IS_DESKTOP ? 96 : 76,
 width: '100%',
-position: 'relative',
-},
-
-headerInner: {
-flexDirection: 'row',
-alignItems: 'center',
-justifyContent: 'flex-end',
-paddingLeft: 0,
-paddingRight: IS_DESKTOP ? 28 : 16,
-paddingVertical: IS_DESKTOP ? 6 : 6,
-minHeight: IS_DESKTOP ? 110 : 82,
-width: '100%',
-position: 'relative',
 },
 
 logo: {
-position: 'absolute',
-left: IS_DESKTOP ? -35 : -12,
-top: 0,
-bottom: 0,
-height: '100%',
+height: IS_DESKTOP ? 88 : 68,
 alignItems: 'flex-start',
 justifyContent: 'center',
+flexShrink: 0,
 },
 
+logoMark: {
+width: IS_DESKTOP ? 280 : 190,
+height: IS_DESKTOP ? 88 : 64,
+},
 
 
   headerNav: {
